56706ebce58d5d9ff82a7d184d8b62ce6a2ffa70
Teeha Scott <teeha.scott@gmail.com>
2024-11-04 13:29:38 -0500

    first commit

---
+++ guided practice/script2.js
@@ -0,0 +1,23 @@
+//get a toothbrush from the store
+/* rip it open with your bare teeth
+howl
+show the toothbrush you are its god by boiling it
+for at least 5 minutes*/
+//get the toothbrush wet using the shower
+//turn the shower off
+//put toothpaste on the brush
+//turn the shower back on
+//wet the brush
+//gnaw on the brush until you recieve your desired whiteness
+// ?? profit
+const cownumber = 50;
+const name = "hello, world";
+console.log("and then I said " + name + "?");
+console.log("Hellow World");
+console.log("Hello? " + "We are at " + "Disney Land.");
+const umbrellanumber = 10 * 3;
+var number = 5 + 1;
+console.log(number);
+const str1 = "Hello" + " " + "World";
+const str2 = "Hello" + " " + "I am" + " " + "Josh";
+alert(str2);
---
+++ javademo/script.js
@@ -0,0 +1,19 @@
+/*console.log("Hello Nepeta!");
+alert;
+("ur hacked now");
+const language = "javascript";
+console.log("I love " + language);
+const str1 = "Fullstack";
+const str2 = "Academy";
+console.log("welcome to" + " " + str1 + " " + str2 + "!");
+console.log(`Welcome to ${str1} ${str2}!`);
+let cat = "nepeta";
+console.log(typeof cat);
+let a = true;
+let b = true;
+let answer = a && b;
+console.log(answer);*/
+console.log(5 > 9);
+//console.log(8>3);
+//console.log(2 == "2");
+//let message = "this is the code";
