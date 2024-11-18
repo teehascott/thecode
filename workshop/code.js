3ad1b54d95bc450440f7a5053023eb0d094d1197
Teeha Scott <teehascott@gmail.com>
2024-11-18 04:53:44 -0500

    initial commit

---
+++ workshop/code.js
@@ -0,0 +1,34 @@
+// first message about the vault
+const message = "You have received this message because you have been chosen to open an important vault.";
+
+// second pop up for more suspense
+const message2 = "Here is the secret combination: ";
+
+// calculations for the code:
+
+//should equal 10 
+const code1 = 100 / 10; 
+
+//should equal 40
+const code2 = 50 * 2 - 80;  
+
+//should equal 39
+const code3 = 15 * 2 + 9;  
+
+// this will display the algorithm answers in the alert
+const crackedCode = `${code1} - ${code2} - ${code3}`;
+
+//ominous message
+const message3 = "Now go save the world..."
+
+//popup for intro
+alert(message);
+
+//popup for second part of intro (suspense)
+alert(message2);
+
+// T H E  C O D E
+alert(crackedCode);
+
+// go get em champ
+alert(message3);
